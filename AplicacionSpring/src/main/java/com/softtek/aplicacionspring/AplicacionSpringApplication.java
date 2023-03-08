033d6887e9440498ef91a5f490b1a8f1319ccf49
Rigoberto Hernandez <Rigoberto Hernandez@LAPTOP-J2VC6MUC>
2023-03-08 14:58:58 -0600

    Primer commit

---
+++ AplicacionSpring/src/main/java/com/softtek/aplicacionspring/AplicacionSpringApplication.java
@@ -0,0 +1,13 @@
+package com.softtek.aplicacionspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplicacionSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplicacionSpringApplication.class, args);
+	}
+
+}
---
+++ AplicacionSpring/src/test/java/com/softtek/aplicacionspring/AplicacionSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softtek.aplicacionspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplicacionSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
